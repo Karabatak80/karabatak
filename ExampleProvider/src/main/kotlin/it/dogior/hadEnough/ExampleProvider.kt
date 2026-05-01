d02837fb7908848b803ea5d779a53b737bf7df2a
Karabatak80 <141875326+Karabatak80@users.noreply.github.com>
2026-05-01 19:31:33 +0300

    Update ExampleProvider.kt

--- ExampleProvider/src/main/kotlin/it/dogior/hadEnough/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/it/dogior/hadEnough/ExampleProvider.kt
@@ -1,54 +1,22 @@
-package it.dogior.hadEnough
+package com.lagradost
 
-import android.util.Log
-import com.lagradost.cloudstream3.Episode
-import com.lagradost.cloudstream3.HomePageResponse
-import com.lagradost.cloudstream3.LoadResponse
-import com.lagradost.cloudstream3.TvType
-import com.lagradost.cloudstream3.MainAPI
-import com.lagradost.cloudstream3.MainPageRequest
-import com.lagradost.cloudstream3.SearchResponse
-import com.lagradost.cloudstream3.SubtitleFile
-import com.lagradost.cloudstream3.mainPageOf
-import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.app
-import org.jsoup.nodes.Document
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.*
 
-
-class ExampleProvider : MainAPI() {
-    override var mainUrl = ""
-    override var name = "Example"
-    override val supportedTypes = setOf(TvType.Movie)
-    override var lang = "it"
+class FilmModu : MainAPI() { 
+    override var name = "FilmModu"
+    override var mainUrl = "https://www.filmmodu.one"
     override val hasMainPage = true
+    override var lang = "tr"
 
-    override val mainPage = mainPageOf(
-        mainUrl to "Home",
-    )
-
-    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        TODO("Not yet implemented")
-    }
-
-    // this function gets called when you search for something
-    override suspend fun search(query: String): List<SearchResponse> {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun load(url: String): LoadResponse {
-        TODO("Not yet implemented")
-    }
-
-    private fun getEpisodes(page: Document): List<Episode> {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun loadLinks(
-        data: String,
-        isCasting: Boolean,
-        subtitleCallback: (SubtitleFile) -> Unit,
-        callback: (ExtractorLink) -> Unit,
-    ): Boolean {
-        TODO("Not yet implemented")
+    override suspend fun getMainPage(page: Int, request: HomePageRequest): HomePageResponse {
+        val document = app.get(mainUrl).document
+        val home = document.select("div.movie-item").mapNotNull {
+            val title = it.selectFirst("h3")?.text() ?: return@mapNotNull null
+            val href = it.selectFirst("a")?.attr("href") ?: return@mapNotNull null
+            val poster = it.selectFirst("img")?.attr("src")
+            newMovieSearchResponse(title, href, TvType.Movie) { this.posterUrl = poster }
+        }
+        return newHomePageResponse("Ana Sayfa", home)
     }
 }
