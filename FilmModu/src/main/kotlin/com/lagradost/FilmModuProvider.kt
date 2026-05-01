f6e6044edc0425a9c3686701240be880c07052c9
Karabatak80 <141875326+Karabatak80@users.noreply.github.com>
2026-05-01 19:25:01 +0300

    Create FilmModuProvider.kt

---
+++ FilmModu/src/main/kotlin/com/lagradost/FilmModuProvider.kt
@@ -0,0 +1,22 @@
+package com.lagradost
+
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.*
+
+class FilmModuProvider : MainAPI() { 
+    override var name = "FilmModu"
+    override var mainUrl = "https://www.filmmodu.one"
+    override val hasMainPage = true
+    override var lang = "tr"
+
+    override suspend fun getMainPage(page: Int, request: HomePageRequest): HomePageResponse {
+        val document = app.get(mainUrl).document
+        val home = document.select("div.movie-item").mapNotNull {
+            val title = it.selectFirst("h3")?.text() ?: return@mapNotNull null
+            val href = it.selectFirst("a")?.attr("href") ?: return@mapNotNull null
+            val poster = it.selectFirst("img")?.attr("src")
+            newMovieSearchResponse(title, href, TvType.Movie) { this.posterUrl = poster }
+        }
+        return newHomePageResponse("Ana Sayfa", home)
+    }
+}
